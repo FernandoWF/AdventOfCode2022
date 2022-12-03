ba85b64b09e2df04038239c498d895144326318a
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2022-12-03 02:47:31 -0300

    Add day 3 part 2 solution

--- Day3/Part2.cs
+++ Day3/Part2.cs
@@ -6,7 +6,26 @@ namespace Day3
     {
         public static void Run()
         {
+            var lines = File.ReadAllLines("Input.txt");
+            var rucksacks = lines
+                .Select(l => new Rucksack(l))
+                .ToList();
 
+            var elvesPerGroup = 3;
+            var badges = rucksacks
+                .Chunk(elvesPerGroup)
+                .Select(r =>
+                {
+                    var badgeLetter = r[0].ItemTypes
+                        .Intersect(r[1].ItemTypes)
+                        .Intersect(r[2].ItemTypes)
+                        .Single();
+
+                    return new Item(badgeLetter);
+                })
+                .ToList();
+
+            Console.WriteLine(badges.Sum(b => b.Priority));
         }
     }
 }
--- Day3/Rucksack.cs
+++ Day3/Rucksack.cs
@@ -3,6 +3,7 @@
     internal class Rucksack
     {
         public Item CommonItem { get; }
+        public ISet<char> ItemTypes { get; } = new HashSet<char>();
 
         public Rucksack(string contents)
         {
@@ -16,7 +17,9 @@
             for (var i = 0; i < compartmentItemCount; i++)
             {
                 firstCompartmentItemTypes.Add(firstCompartmentContents[i]);
+                ItemTypes.Add(firstCompartmentContents[i]);
                 secondCompartmentItemTypes.Add(secondCompartmentContents[i]);
+                ItemTypes.Add(secondCompartmentContents[i]);
             }
 
             var commonItemKind = firstCompartmentItemTypes
